c3bf9d6b2f28584be3df9473dc2af8f00963ef61
SAIKAT TANTI <saikat.tanti.fiem.cse23@teamfuture.in>
2025-01-24 00:45:37 +0530

    Form Page

--- components/AuthFrom.tsx
+++ components/AuthFrom.tsx
@@ -1,24 +1,50 @@
+'use client';
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
-const AuthFrom = ({ type }: { type: string }) => {
+const AuthForm = ({ type }: { type: string }) => { const [user, setUser] = useState(null)
   return (
-    <section className="auth-from">
+    <section className="auth-form">
       <header className='flex flex-col gap-5 md:gap-8'>
-        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-1 px-4">
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
           <Image
             src="/icons/logo.svg"
             width={34}
             height={34}
             alt="Horizon logo"
-            
           />
           <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
         </Link>
+
+        <div className="flex flex-col gap-5 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user
+              ? 'Link Account'
+              : type === 'sign-in'
+                ? 'Sign In'
+                : 'Sign Up'
+            }
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? 'Lin k your account to continue'
+                : 'Please enter your details to continue'
+                }
+            </p>
+          </h1>
+        </div>
       </header>
+      {user ? (
+        <div className="flex flex-col gap-4">
+        {/*PlaidLink */}
+      </div>
+       ): (
+        <>
+          FORM        
+        </>
+       )}
     </section>
   )
 }
 
-export default AuthFrom
+export default AuthForm
